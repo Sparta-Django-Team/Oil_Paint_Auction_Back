030e44436ef61852d71f0cf49ccdb6aeba2c58b3
Jay <jvidorca@gmail.com>
2022-11-22 22:50:35 +0900

    Feat:add urls.py_paintings

--- project_back/paintings/urls.py
+++ project_back/paintings/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('styleselect/', views.PaintingStyleSelectView.as_view(), name='styleselect_view'),
+    path('<int:style_no>/', views.PaintingCreateView.as_view(), name='paintingcreate_view'),
 ]
