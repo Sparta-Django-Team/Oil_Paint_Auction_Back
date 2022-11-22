c14263de84edb1a4f4c4b92c76b1d25701f59ab8
Jay <jvidorca@gmail.com>
2022-11-22 22:49:33 +0900

    Feat:add views_paintings

--- project_back/paintings/views.py
+++ project_back/paintings/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+
+from django.shortcuts import get_list_or_404
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from paintings.models import Painting, PaintStyle
+
+
 
 # Create your views here.
+class PaintingStyleSelectView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class PaintingCreateView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request, style_no):
+        pass
+
+    def post(self, request):
+        pass
