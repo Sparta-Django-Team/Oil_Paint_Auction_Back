7085d909d56a8cc269f0fef51e2618c4e1376faa
Jay <jvidorca@gmail.com>
2022-11-22 22:50:06 +0900

    Feat:add serializers_paintings

---
+++ project_back/paintings/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Painting, PaintStyle
+
+
+class StyleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaintStyle
+        fields = ('model_name', 'model_urls',)
+
+
+class PaintingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Painting
+        fields = "__all__"
