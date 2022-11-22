0e325a81e448bfad4d3cc749dfaea5c1fc3f07d5
Jay <jvidorca@gmail.com>
2022-11-22 22:41:30 +0900

    Feat:add models in paintings

--- project_back/paintings/models.py
+++ project_back/paintings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class PaintStyle(models.Model):
+    model_name = models.CharField(max_length=70, blank=True)
+    model_urls = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return str(self.model_name)
+
+class Painting(models.Model):
+    title = models.CharField(max_length=70, blank=True)
+    content = models.CharField(max_length=250, blank=True)
+    before_image = models.ImageField(blank=True, upload_to='before_img')
+    after_image = models.ImageField(blank=True, upload_to='after_img')
+    paint_style = models.ForeignKey(PaintStyle, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta: 
+        db_table = 'db_painting'
+
+    def __str__(self):
+        return str(self.title) 
+
+
