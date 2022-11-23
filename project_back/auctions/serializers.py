d11a0cca1f459a1f2d473a0d5bf0266e038cc805
jiyeong <2jiyeong0@gmail.com>
2022-11-23 17:22:31 +0900

    Feat : 경매 조회(전체, 상세), 삭제 기능 추가
    - 1. 경매 전체 조회 AuctionAllListView
    - 2. 경매 상세 조회 /  삭제  AuctionDetailView

--- project_back/auctions/serializers.py
+++ project_back/auctions/serializers.py
@@ -9,9 +9,38 @@ class MyPageserializer(serializers.ModelSerializer):
         model = Painting
         fields = "__all__"
 
-
 class AuctionCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Auction
         fields = ("start_bid","end_date",)
+
+class paintingserializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+
+    def get_author(self, obj):
+         return obj.author.email
+
+    class Meta:
+        model = Painting
+        fields = ('id', 'title', 'content', 'author')
+
+class AuctionListSerializer(serializers.ModelSerializer):
+    auction_like = serializers.StringRelatedField(many=True)
+    auction_like_count = serializers.SerializerMethodField()
+    painting = paintingserializer()
+
+    def get_auction_like_count(self, obj) :    
+        return obj.auction_like.count()
+
+    class Meta:
+        model = Auction
+        fields = "__all__"
+
+class AuctionDetailSerializer(serializers.ModelSerializer):
+    auction_like = serializers.StringRelatedField(many=True)
+    painting = paintingserializer()
+
+    class Meta:
+        model = Auction
+        fields = "__all__"
 
--- project_back/auctions/views.py
+++ project_back/auctions/views.py
@@ -6,23 +6,50 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from django.shortcuts import get_list_or_404
 
-from .serializers import MyPageserializer,AuctionCreateSerializer
+from .serializers import MyPageserializer,AuctionCreateSerializer, AuctionListSerializer, AuctionDetailSerializer
 from .models import Painting, Auction
 
 
 #유화 리스트페이지
 class MyPageView(APIView):
     permission_classes = [IsAuthenticated] 
-    def get(self, request):
-        painting = get_list_or_404(Painting, author=request.user.id)        
+    def get(self, request ,user_id):
+        painting = get_list_or_404(Painting, author=user_id)        
         serializer = MyPageserializer(painting, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# 경매 전체 리스트
+class AuctionAlllistView(APIView):
+    permissions_classes = [AllowAny] 
+    def get(self, request):
+        auction = Auction.objects.all()
+        serializer = AuctionListSerializer(auction, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# 경매 상세 조회
+class AuctionDetailView(APIView):
+    permissions_classes = [AllowAny] 
+    def get(self, request,user_id, auction_id):
+        auction = get_object_or_404(Auction, id=auction_id)
+        print(auction)
+        serializer = AuctionDetailSerializer(auction)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    # 경매 삭제
+    def delete(self, request, user_id, auction_id):
+        auction = get_object_or_404(Auction, id=auction_id)
+        if request.user == auction.painting.author:
+            auction.delete()
+            return Response(status=status.HTTP_200_OK)
+        return Response("접근 권한 없음", status=status.HTTP_403_FORBIDDEN) 
 
 
 #경매 생성
 class AuctionListView(APIView):
     permission_classes = [IsAuthenticated]
-    def post(self,request,painting_id):       
+    def post(self,request,painting_id):
         serializer = AuctionCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(painting_id=painting_id)
