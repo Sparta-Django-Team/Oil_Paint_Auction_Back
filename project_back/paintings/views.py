76032b6bc0ce379364979979107253966c027b76
Jay <jvidorca@gmail.com>
2022-11-24 22:54:17 +0900

    Fix:Fix some typo_auction, paintings

--- project_back/paintings/views.py
+++ project_back/paintings/views.py
@@ -1,5 +1,3 @@
-
-
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
@@ -9,8 +7,7 @@ from rest_framework.generics import get_object_or_404
 from django.shortcuts import get_list_or_404
 
 from paintings.models import Painting
-from paintings.serializers import (PaintingSerializer, PaintingCreateSerializer, ImageSerializer, 
-                                PaintingListSerializer)
+from paintings.serializers import (PaintingListSerializer, PaintingCreateSerializer, ImageSerializer)
 from .models import Painting, STYLE_CHOICES
 
 
@@ -24,16 +21,18 @@ class PaintingListview(APIView):
         serializer = PaintingListSerializer(painting, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
 class PaintingStyleSelectView(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     #유화 스타일 선택 페이지
     def get(self, requets):
         style = [[x, y] for x, y in STYLE_CHOICES]
         return Response(style, status=status.HTTP_200_OK)
 
+
 class ImageUploadView(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     
     #유화 스타일 생성 페이지
     def get(self, requets):
@@ -48,9 +47,9 @@ class ImageUploadView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class PaintingCreateView(APIView):
     permission_classes = [IsAuthenticated]
-    
     #이미지 스타일 적용된 유화 생성(after)
     def put(self, request, painting_id):
         painting = get_object_or_404(Painting, id=painting_id)
@@ -61,7 +60,6 @@ class PaintingCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class PaintingDetailView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -69,7 +67,7 @@ class PaintingDetailView(APIView):
     def put(self, request, painting_id):        
         painting = get_object_or_404(Painting, id=painting_id)
         if request.user == painting.author:
-            serializer = PaintingSerializer(painting, data=request.data)
+            serializer = PaintingListSerializer(painting, data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
