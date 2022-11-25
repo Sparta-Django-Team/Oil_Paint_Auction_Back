0fed0b4f31616f9437954406d11c10630cdc8687
woong-garden <dndwn7007@gmail.com>
2022-11-25 17:43:13 +0900

    수정

--- project_back/paintings/urls.py
+++ project_back/paintings/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('style/', views.PaintingStyleSelectView.as_view(), name='style_select_view'),
     path('img/', views.ImageUploadView.as_view(), name='image_upload_view'),
     path('img/<int:painting_id>/', views.PaintingCreateView.as_view(), name='painting_create_view'),
+    # path('<int:painting_id>/',)
 ]
