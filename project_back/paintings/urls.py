eccac17d975fcdfcd51980ab85f5e400b6804d74
woong-garden <dndwn7007@gmail.com>
2022-11-25 18:40:14 +0900

    pull commit

--- project_back/paintings/urls.py
+++ project_back/paintings/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('<int:painting_id>/', views.PaintingDetailView.as_view(), name='painting_detail_view'),
     path('style/', views.PaintingStyleSelectView.as_view(), name='style_select_view'),
     path('img/', views.ImageUploadView.as_view(), name='image_upload_view'),
-<<<<<<< HEAD
     path('img/<int:painting_id>/', views.PaintingCreateView.as_view(), name='painting_create_view'),
-    # path('<int:painting_id>/',)
-=======
->>>>>>> 9e9b5613c16b82d4d6a0ca7d66b364d1a740ac82
+
 ]
