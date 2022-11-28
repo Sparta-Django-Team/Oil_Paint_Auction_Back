9bf54b7ee2afac4719b92413b5a32f81e96464a3
ssa <wogur981208@gmail.com>
2022-11-28 16:48:44 +0900

    Fix : like model null값 오류 취소

--- project_back/auctions/models.py
+++ project_back/auctions/models.py
@@ -10,7 +10,7 @@ class Auction(models.Model):
     end_date = models.DateTimeField('경매 마감', null=True)
 
     painting = models.ForeignKey(Painting, verbose_name="유화",on_delete=models.CASCADE, related_name="paintings")
-    auction_like = models.ManyToManyField(User, verbose_name='경매 좋아요', related_name="like_auction",blank=True, null=True)
+    auction_like = models.ManyToManyField(User, verbose_name='경매 좋아요', related_name="like_auction",blank=True)
     
     bidder = models.ForeignKey(User,  verbose_name='입찰자', on_delete=models.CASCADE, null=True, blank=True, related_name='auction_bidder')
     seller = models.ForeignKey(User,  verbose_name='판매자',on_delete=models.CASCADE, null=True, related_name='auction_seller')    
