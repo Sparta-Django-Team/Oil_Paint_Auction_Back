7d1d8f22fad754b520e2bfea13c3253b584dd8c0
ssa <wogur981208@gmail.com>
2023-03-20 19:37:45 +0900

    Feat: User url 수정

--- project_back/users/urls.py
+++ project_back/users/urls.py
@@ -1,19 +1,30 @@
+# rest_framework
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+
+# django
 from django.urls import path
 
-from rest_framework_simplejwt.views import TokenRefreshView
-
+# users
 from . import views
 
 urlpatterns = [
-    #User
-    path('', views.UserView.as_view(), name='user_view'),
-    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/', views.LogoutAPIview.as_view(), name='logout'),
+    # Auth
+    path("auth/signup/", views.SignupView.as_view(), name="signup"),
+    path("auth/token/", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("auth/logout/", views.LogoutView.as_view(), name="logout"),
     
-    #Password
-    path('changepassword/', views.ChangePasswordView.as_view(), name='change_password_view'),
-    path('password-reset-email/', views.PasswordResetView.as_view(),name="password_reset_email"),
-    path('password-reset/<uidb64>/<token>/',views.PasswordTokenCheckView.as_view(), name='password_reset_confirm'),
-    path('password-reset-complete/', views.SetNewPasswordView.as_view(), name='password_reset_complete'),
+    # # Password
+    path("auth/password/change/", views.PasswordChangeView.as_view(), name="password_change"),
+    path("auth/password/reset/", views.PasswordResetView.as_view(), name="password_reset"),
+    path("auth/password/reset/<uidb64>/<token>/", views.PasswordTokenCheckView.as_view(), name="password_reset_confirm"),
+    path("auth/password/reset/confirm/", views.SetNewPasswordView.as_view(), name="password_reset_confirm"),
+    
+    # Profile
+    path("users/token/", views.ObtainUserTokenView.as_view(), name="user_token"),
+    path("users/", views.UserProfileView.as_view(), name="user_profile"),
+    
+    # # Attendance
+    path("attendance/", views.AttendanceCheckView.as_view(), name="attendance_check"),
 ]
